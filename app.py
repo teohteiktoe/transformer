a75deeeac0611ab2bdd6be43a587fe80b6cde02e
Teoh TeikToe <ttt@mensa.org.sg>
2022-03-19 19:38:50 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+from flask import render_template,request
+
+
+# In[4]:
+
+
+from transformers import pipeline
+classifier = pipeline("sentiment-analysis")
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        text = request.form.get("text")
+        print(text)
+        r = classifier(text)
+        return(render_template("index.html", result=r))
+    else:
+        return(render_template("index.html", result="2"))
+
+
+# In[ ]:
+
+
+if __name__=="__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+get_ipython().system('pip freeze')
+
+
+# In[ ]:
+
+
+
+
